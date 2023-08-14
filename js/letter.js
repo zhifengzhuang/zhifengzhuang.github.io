3bfc55d75e4253931edaa94bb995e689a98a7945
qinghuan <qinghuan@pinduoduo.com>
2023-08-14 15:16:09 +0800

    feat: some optimize

--- js/letter.js
+++ js/letter.js
@@ -5,7 +5,7 @@ $("#open").click(function () {
     if (!aparted) {
         var typed = new Typed('.letter', {
             strings: ["^1000Wei&nbsp;&nbsp;MinYan",
-                "魏^200敏燕<br><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;哈喽呀，不知不觉我们已经认识两个月了，突然就这样有一个人走进了我的生活，或许这就是缘分吧，从第一次见你的时候我就觉得我遇到了一个不一样的女孩，你带给我了欢乐，懂我内心很多想法，未来还有好多好多地方和美食希望和你一起去体验，想跟你说，我喜欢你<br><br><p style='float:right; display:block; width:80px;'>^1000庄^200增锋</p>"],
+                "魏^200敏燕<br><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;哈喽呀，不知不觉我们已经认识两个月了，突然就这样有一个人走进了我的生活，或许这就是缘分吧，从第一次见你的时候我就觉得我遇到了一个不一样的女孩，你带给了我欢乐，懂我内心很多想法，未来还有好多好多地方和美食希望和你一起去体验，想跟你说，我喜欢你<br><br><p style='float:right; display:block; width:80px;'>^1000庄^200增锋</p>"],
             typeSpeed: 300,
             backSpeed: 50
         });
@@ -27,14 +27,10 @@ function refresh() {
     let start = new Date(2023, 5, 22, 20, 20, 20, 0);
     let now = new Date();
     let past = (now.getTime() - start.getTime()) / 1000;
-    document.getElementById('days').innerHTML = Math.ceil(past / 60 / 60 / 24).toString();
-    document.getElementById('hours').innerHTML = Math.ceil(past / 60 / 60 % 24).toString();
-    document.getElementById('minutes').innerHTML = Math.ceil(past / 60 % 60).toString();
-    let seconds = "0" + Math.ceil(past % 60).toString();
-    if (seconds.length == 1) {
-        seconds = "0" + seconds;
-    }
-    document.getElementById('seconds').innerHTML = seconds;
+    document.getElementById('days').innerHTML = Math.floor(past / 60 / 60 / 24).toString();
+    document.getElementById('hours').innerHTML = Math.floor(past / 60 / 60 % 24).toString();
+    document.getElementById('minutes').innerHTML = Math.floor(past / 60 % 60).toString();
+    document.getElementById('seconds').innerHTML = Math.floor(past % 60).toString();
 }
 
 
